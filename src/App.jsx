f7b2939db2fda9c57577cb8a745417cb38723604
GrossoCarla <grossocarlam@gmail.com>
2020-12-10 19:40:23 -0300

    Ordeno carpetas y genero ProductCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './styles/App.css';
 import Hero from './components/Home/Hero';
-import Footer from './components/Global/Footer';
-import NavBar from './components/Global/NavBar';
+import Footer from './components/Global/Footer/Footer';
+import NavBar from './components/Global/NavBar/NavBar';
+import ProductCard from './components/Global/ProductCard/ProductCard'
 import camisaChina from './assets/camisaChina.jpg';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Hero />
         <img src={camisaChina} className='foto-producto' alt='Camisa de corte chino negra'/>
+        <ProductCard/>
         <Footer />
     </>
   );
